e8ca42958a502e8d1cb0014655fdcd80941056a7
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-04-08 12:59:42 +0530

    complete the whole site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 ////////////////////////  NAVBAR ///////////////////////////
+// `use strict`;
+
 const menu = document.querySelector(".menu");
 const navLists = document.querySelector(".nav-links-con");
 
@@ -8,30 +10,44 @@ menu.addEventListener("click", function () {
 
 ////////////////////////  link shortner ///////////////////////////
 const shortenBtn = document.querySelector(".shorten-btn");
-const resultLink = document.querySelector(".result-link");
 const rawLink = document.querySelector(".url-input");
 const displayArea = document.querySelector("#input-section .container .form");
 const forCopyBtn = document.querySelector("#input-section .container");
+const errorMsg = document.querySelector(".error-msg");
 
 function shortenLink(link) {
   fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
     .then((res) => {
+      if (!res.ok) throw new Error(`${res.status} Invalid link`);
       return res.json();
     })
     .then((data) => {
       console.log(data);
       renderDisplayForm(data.result.short_link);
+    })
+    .catch((err) => {
+      displayErrorMsg(err);
     });
 }
 
+function displayErrorMsg(err) {
+  errorMsg.classList.remove("hidden");
+  errorMsg.textContent = `${err} `;
+}
+
 function renderDisplayForm(link) {
-  const html = `<div class="result-link">
+  const html = `
+  <div class="result-link">
+    <div class="link-con">
       <h2 class="link">${rawLink.value}</h2>
-      <div class=" right">
-        <h2 class="shorten-link">${link}</h2>
-        <button class="btn btn-color btn-lg btn-sq copy">copy</button>
-      </div>
-    </div>`;
+    </div>
+    <div class="right ">
+      <h2 class="shorten-link">${link}</h2>
+      <button class="btn btn-color btn-lg btn-sq copy">copy</button>
+    </div>
+  </div>
+  `;
+
   displayArea.insertAdjacentHTML("afterend", html);
   rawLink.value = "";
 }
@@ -39,12 +55,37 @@ function renderDisplayForm(link) {
 // event listners
 shortenBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  shortenLink(rawLink.value);
+  if (rawLink.value == "") {
+    rawLink.classList.add("error");
+    errorMsg.classList.remove("hidden");
+  } else {
+    shortenLink(rawLink.value);
+    rawLink.classList.remove("error");
+    errorMsg.classList.add("hidden");
+  }
 });
 
 forCopyBtn.addEventListener("click", function (e) {
   if (e.target.classList.contains("copy")) {
     e.target.style.backgroundColor = "hsl(257, 27%, 26%)";
+
+    setTimeout(() => {
+      e.target.style.backgroundColor = "hsl(180, 66%, 49%)";
+      e.target.textContent = "copy";
+    }, 3000);
     e.target.textContent = "copied !";
+    copyToClipBoard(e.target);
   }
 });
+
+function copyToClipBoard(btn) {
+  const copyText = btn.closest(".right").querySelector(".shorten-link");
+  const textArea = document.createElement("textarea");
+  textArea.setAttribute("readonly", "");
+  textArea.style.position = "absolute";
+  textArea.value = copyText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.classList.add("hidden");
+}
